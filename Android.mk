808bab8d2f01972041131815d3cdf92fc365c75e
CannedShroud <hypersync112@gmail.com>
2022-01-18 21:29:29 +0530

    KProfiles: Move init files dep to blueprint
    Signed-off-by: CannedShroud <hypersync112@gmail.com>

--- Android.mk
+++ Android.mk
@@ -22,8 +22,5 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     KProfiles
 
-PRODUCT_COPY_FILES += \
-	packages/apps/KProfiles/init/init.kprofiles.rc:$(TARGET_OUT_VENDOR_ETC)/init/hw/init.kprofiles.rc
-
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
      packages/apps/KProfiles/sepolicy/private
