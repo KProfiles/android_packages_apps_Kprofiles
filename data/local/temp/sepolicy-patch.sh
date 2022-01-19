ffed6158af1258dc0fd7484b738789dd64482b59
CannedShroud <hypersync112@gmail.com>
2022-01-19 20:49:12 +0530

    KProfiles: Write correct rules to be injected
    Signed-off-by: CannedShroud <hypersync112@gmail.com>

--- data/local/temp/sepolicy-patch.sh
+++ data/local/temp/sepolicy-patch.sh
@@ -3,4 +3,4 @@ echo "" > /system/etc/selinux/plat_and_mapping_sepolicy.cil.sha256
 
 # Inject necessary rules
 chmod +x /data/local/tmp/sepolicy-inject
-/data/local/tmp/sepolicy-inject -s system_app -t sysfs -c file -p write -P /vendor/etc/selinux/precompiled_sepolicy -l
+/data/local/tmp/sepolicy-inject -s system_app -t sysfs -c file -p read,write,open,getattr -P /vendor/etc/selinux/precompiled_sepolicy -l
