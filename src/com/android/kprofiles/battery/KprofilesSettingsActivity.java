1bb37a26f7b1ddc4878bfb86f3f197a2bb4a8fec
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-06 08:13:57 +0200

    KprofilesSettingsActivity: Adapt to android-13.0.0_r16

--- src/com/android/kprofiles/battery/KprofilesSettingsActivity.java
+++ src/com/android/kprofiles/battery/KprofilesSettingsActivity.java
@@ -19,7 +19,7 @@ package com.android.kprofiles.battery;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class KprofilesSettingsActivity extends CollapsingToolbarBaseActivity {
 
