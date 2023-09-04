8c2fbd1853eed5585d56c411e364b64eefd4e588
Dakkshesh <dakkshesh5@gmail.com>
2023-09-04 21:38:27 +0530

    Kprofiles: Update for kp-6.0.0
    The sysfs nodes were changed in kp-6.0.0, Update the paths respectively.
    
    Change-Id: I016c78e38f8392af50cb03dc79ba2387ac9d70da
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/com/android/kprofiles/battery/KprofilesSettingsFragment.java
+++ src/com/android/kprofiles/battery/KprofilesSettingsFragment.java
@@ -47,9 +47,9 @@ public class KprofilesSettingsFragment extends PreferenceFragment implements
     private boolean mSelfChange = false;
 
     public static final String INTENT_ACTION = "com.android.kprofiles.battery.KPROFILE_CHANGED";
-    public static final String KPROFILES_MODES_NODE = "/sys/module/kprofiles/parameters/kp_mode";
+    public static final String KPROFILES_MODES_NODE = "/sys/kernel/kprofiles/kp_mode";
     public static final String KPROFILES_AUTO_KEY = "kprofiles_auto";
-    public static final String KPROFILES_AUTO_NODE = "/sys/module/kprofiles/parameters/auto_kprofiles";
+    public static final String KPROFILES_AUTO_NODE = "/sys/module/kprofiles/parameters/auto_kp";
     public static final String KPROFILES_MODES_KEY = "kprofiles_modes";
     public static final String KPROFILES_MODES_INFO = "pref_kprofiles_modes_info";
     public static final String ON = "Y";
